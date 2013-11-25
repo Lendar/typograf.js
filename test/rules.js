b9f2e270a2eeb8ee567b091c6d07850d94ff8ae4
Lendar <xeldim@gmail.com>
2013-11-25 04:31:33 +0400

    add mdash rule

--- test/rules.js
+++ test/rules.js
@@ -1,4 +1,8 @@
+var should = require('should');
+var rules = require('../lib/rules');
+
 describe('rules', function(){
-  it('should work', function(){
+  it('should fix dash', function(){
+    rules.mdash('a - b').should.equal('a — b');
   });
 });
